513623903c858d3a816b9197b1f46902e670391a
Drag-3 <juserysthee@gmail.com>
2023-05-18 14:11:22 -0400

    make logger create all dirs to location

--- mporg/main.py
+++ mporg/main.py
@@ -114,7 +114,7 @@ def set_logging(v: bool):
     logger.setLevel(logging.DEBUG if v else logging.INFO)
 
     if not LOG_DIR.exists():
-        LOG_DIR.mkdir(0o666)
+        LOG_DIR.mkdirs(0o666)
 
     addLoggingLevel("TOP", logging.CRITICAL - 1)
     # Create a formatter
