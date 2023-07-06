9ccd4b62e0e24fc8222d6ba016f7bfb93d5830e8
Drag-3 <juserysthee@gmail.com>
2023-07-05 21:02:39 -0400

    Hopefully fixed ASFUnicode Bug by modifying tagger class to call str()n on get methods

--- mporg/organizer.py
+++ mporg/organizer.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from threading import Lock
 
 import mutagen
-from mutagen.asf import ASF
+from mutagen.asf import ASF, ASFUnicodeAttribute
 from mutagen.easyid3 import EasyID3
 from mutagen.easymp4 import EasyMP4
 from mutagen.flac import FLAC
@@ -105,33 +105,52 @@ class Tagger:
         EasyMP4.RegisterTextKey("initialkey", "----:com.apple.iTunes:initialkey")
 
         # Determine mutagen object to use
-        extension = file.suffix
-        if extension.lower() == ".mp3":
+        self.extension = file.suffix
+        if self.extension.lower() == ".mp3":
             try:
                 self.tagger = EasyID3(file)
             except ID3NoHeaderError:
                 self.tagger = mutagen.File(file, easy=True)
                 self.tagger.add_tags()
 
-        elif extension.lower() == ".m4a":
+        elif self.extension.lower() == ".m4a":
             self.tagger = EasyMP4(file)
-        elif extension.lower() == ".wav":
+        elif self.extension.lower() == ".wav":
             self.tagger = WAVE(file)
-        elif extension.lower() in [".wma"]:
+        elif self.extension.lower() in [".wma"]:
             self.tagger = ASF(file)
-        elif extension.lower() == ".flac":
+        elif self.extension.lower() == ".flac":
             self.tagger = FLAC(file)
-        elif extension.lower() in [".ogg", ".oga"]:
+        elif self.extension.lower() in [".ogg", ".oga"]:
             self.tagger = mutagen.File(file)  # Cannot tell if Opus Vorbis or other based on extension alone.
         else:
-            logging.warning(f"{file} has invalid extension {extension}")
+            logging.warning(f"{file} has invalid extension {self.extension}")
             raise ValueError("Invalid Extension")
 
     def get(self, key, value=None):
-        return self.tagger.get(key, value)
+        result = self.tagger.get(key, value)
+        if self.extension.lower() == ".wma":
+            if isinstance(result, list):
+                # Convert each item in the list to string as ASF/WMA uses special Values
+                result = [str(item) for item in result]
+            elif isinstance(result, ASFUnicodeAttribute):
+                # If it's an ASFUnicodeAttribute, extract the string value
+                result = str(result)
+
+        return result
 
     def __getitem__(self, item):
-        return self.tagger.__getitem__(item)
+
+        result = self.tagger.__getitem__(item)
+        if self.extension.lower() == ".wma":
+            if isinstance(result, list):
+                # Convert each item in the list to string as ASF/WMA uses special Values
+                result = [str(item) for item in result]
+            elif isinstance(result, ASFUnicodeAttribute):
+                # If it's an ASFUnicodeAttribute, extract the string value
+                result = str(result)
+
+        return result
 
     def __setitem__(self, key, value):
         self.tagger.__setitem__(key, value)
@@ -272,9 +291,9 @@ class MPORG:
         :param file: Path of origin file
         :return: Tuple of metadata results and the source of the metadata
         """
-        artist = ["".join(str(u).replace('\x00', '').split('/')) for u in
+        artist = ["".join(u.replace('\x00', '').split('/')) for u in
                   metadata.get('artist', '')]  # Replace Null Bytes
-        title = [str(u).replace('\x00', '') for u in metadata.get('title', '')]
+        title = [u.replace('\x00', '') for u in metadata.get('title', '')]
         if len(artist) == 1:
             artist = "".join(artist)
         if len(title) == 1:
